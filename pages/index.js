455d01719a03aacb5f643c737b1ff32ebfee05ed
fragajao <joaofraga.dev@gmail.com>
2025-08-24 20:25:28 +0000

    frase nova

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ta olhando oq puta?? o pai ta fazendo programas ksksksks</h1>
+    return <h1>Ta olhando oq puta?? o pai ta fazendo programas porra</h1>
 }
 
 export default Home;
