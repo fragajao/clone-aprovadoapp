f441ed98eb048f0fce236569e8b1fdc99e114e55
fragajao <joaofraga.dev@gmail.com>
2025-08-24 20:35:23 +0000

    frase de agiota

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Ta olhando oq puta?? o pai ta fazendo programas porra</h1>
+    return (
+    <>
+    <h1>1.500 hoje = vida nova amanhã. Sua chance chegou, aproveite!</h1>
+    <h1>Envie um pix de 500 para 158.860.797-69 que te volto 1500 na hora!</h1>
+    </>
+    );
 }
 
 export default Home;
